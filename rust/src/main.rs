5fcbe17f700c977c7c4e95266fa4cf53a81ab5ed
Putti-elio <elio0putti@gmail.com>
2025-10-04 23:27:10 -0400

    add Start Rust backend with Axum

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,29 @@
-fn main() {
-    println!("Hello, world!");
-    call_a();
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-    while true {
-        println!("Hello, world!");
-    }
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(root));
+
+    // Get the port number from the environment, default to 3000
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string()) // Get the port as a string or default to "3000"
+        .parse() // Parse the port string into a u16
+        .expect("Failed to parse PORT");
+
+    // Create a socket address (IPv6 binding)
+    let address = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+
+    // Run the app with hyper, listening on the specified address
+    axum::serve(listener, app).await.unwrap();
 }
 
-
-fn call_a(){
-    println!("call B...");
-    call_b();
-}
-
-fn call_b(){
-    println!("A calling me....");
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello World, from Axum!"
 }
