ff16b6e2d0d9d00275f6e875a66272f219ef419c
Putii-elio <elio0putti@gmail.com>
2026-02-14 03:04:50 -0500

    feat(routes): add REST endpoints for albums
    - GET /albums - list all albums
    - GET /albums/:id - get single album
    - POST /albums - create album
    - PUT /albums/:id - update album
    - DELETE /albums/:id - delete album
    - GET /artists/:id/albums - list artist albums

--- rust/src/routes/mod.rs
+++ rust/src/routes/mod.rs
@@ -4,16 +4,26 @@ use rusqlite::Connection;
 
 
 mod artist_route;
+pub mod album_route;
 
 pub type SharedDatabase = Arc<Mutex<Connection>>;
 
 pub fn create_router(database: SharedDatabase) -> Router {
     Router::new()
+        // Artists routes
         .route("/artists", get(artist_route::get_all_artists))
         .route("/artists/{id}", get(artist_route::get_artist))
         .route("/artists", post(artist_route::create_artist))
         .route("/artists/{id}", put(artist_route::update_artist))
         .route("/artists/{id}", delete(artist_route::delete_artist))
+        // Albums routes
+        .route("/albums", get(album_route::get_all_albums))
+        .route("/albums/{id}", get(album_route::get_album))
+        .route("/albums", post(album_route::create_album))
+        .route("/albums/{id}", put(album_route::update_album))
+        .route("/albums/{id}", delete(album_route::delete_album))
+        // Artist albums nested route
+        .route("/artists/{id}/albums", get(album_route::get_albums_by_artist))
         .with_state(database)
 }
 
